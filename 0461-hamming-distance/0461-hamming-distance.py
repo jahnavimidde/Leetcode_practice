998c8eb7e8cffd98cdf67a148b5daecd19adcdfb
Midde Jahnavi <mjahnavi956@gmail.com>
2026-01-03 15:32:57 +0530

    Time: 0 ms (100%), Space: 12.3 MB (82.37%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        res=x^y
+        count=0
+        while res:
+            res=res&(res-1)
+            count+=1
+        return count
+        
