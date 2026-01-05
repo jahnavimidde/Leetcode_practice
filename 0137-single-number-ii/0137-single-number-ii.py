aff0fb7e39598153d1971aa99410f704b134094c
Midde Jahnavi <mjahnavi956@gmail.com>
2026-01-05 13:04:54 +0530

    Time: 1 ms (79.67%), Space: 13.5 MB (71.68%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -4,24 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        #better solution 
-        #count te i th bit of each number check if i th bit in each num is multiple of three ....means [5,5,5,2,4,4,4]
-        #101
-        #101
-        #101
-        #010
-        #100
-        #100   by checking 0th bit of 5 2 4 there are 3 one bits then single number 0th bit is '0' like wise we need to do for 1 and 2 bit also 
-        ans=0
-        for bitindex in range(0,32):
-            count=0
-            for num in nums:
-                
-                if num &(1<<bitindex):
-                    count+=1
-            if count%3==1:
-                    ans=ans|(1<<bitindex)
-        if ans>=2**31:
-            ans-=2**32
-        return ans 
-                    
+        #sort array 1 1 1 2 2 2 3 4 4 4 check at index 1 compare before ele 
+        # if same or not.  same ? move to next index+3 pos and check .else return index-1
+        # edge cases if element at last we cant find it so after loop end we simply return last . 
+        nums.sort()
+        low=1
+        high=len(nums)-1
+        while low<=high:
+            if nums[low]==nums[low-1]:
+                low+=3
+            else:
+                return nums[low-1]
+        return nums[high]
