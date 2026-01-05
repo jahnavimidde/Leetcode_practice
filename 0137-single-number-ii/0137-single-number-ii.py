df8185e0ff92d734d56315c10f038b545fbb72a6
Midde Jahnavi <mjahnavi956@gmail.com>
2026-01-05 13:23:24 +0530

    Time: 0 ms (100%), Space: 14 MB (33.24%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -4,15 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        #sort array 1 1 1 2 2 2 3 4 4 4 check at index 1 compare before ele 
-        # if same or not.  same ? move to next index+3 pos and check .else return index-1
-        # edge cases if element at last we cant find it so after loop end we simply return last . 
-        nums.sort()
-        low=1
-        high=len(nums)-1
-        while low<=high:
-            if nums[low]==nums[low-1]:
-                low+=3
-            else:
-                return nums[low-1]
-        return nums[high]
+        ones=0
+        twos=0
+        for i in range(len(nums)):
+            ones=(ones^nums[i])&(~twos)
+            twos=(twos^nums[i])&(~ones)
+        return ones
