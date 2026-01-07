fb5e1f970243158a2f362ca2f258181495c5bb62
Midde Jahnavi <mjahnavi956@gmail.com>
2026-01-07 23:03:18 +0530

    Time: 4 ms (80.17%), Space: 12.5 MB (8.5%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -5,13 +5,7 @@ class Solution(object):
         :type right: int
         :rtype: int
         """
-        #right shift the nums until they are equal .because if the bits in them differ means(0,1) AND of those bits will be zero..so count hoe many BITS GONNNA BE ZERO >>PADDING THE NO OF THE ZEROES BY LEFT SHIFTING AGAIN..SMART!
-        count=0
-        while left!=right:
-            left=left>>1
-            right=right>>1
-            count+=1
-        ans=left&right
-        ans=ans<<count
-        return ans
-        
+        #optimal solution ..see in notebook 
+        while right>left:
+            right=right&(right-1)
+        return right
