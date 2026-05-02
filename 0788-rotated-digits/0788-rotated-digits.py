886a7349cace80778d3b673e48e6f7174d0d4366
Midde Jahnavi <mjahnavi956@gmail.com>
2026-05-03 00:07:15 +0530

    Time: 75 ms (26.47%), Space: 12.7 MB (38.24%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def rotatedDigits(self, n):
+        count = 0
+        
+        for i in range(1, n + 1):
+            s = str(i)
+            
+            
+            if any(d in s for d in "347"):
+                continue
+            
+           
+            if any(d in s for d in "2569"):
+                count += 1
+                
+        return count
+
+           
+        
+        
+
+        
+                
+
+
+        
