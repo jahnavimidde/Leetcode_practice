2540c2fa0c4a85e227ab053e683a92b9adfeb077
Midde Jahnavi <mjahnavi956@gmail.com>
2026-06-21 16:04:50 +0530

    Time: 7 ms (76.67%), Space: 18.6 MB (50.09%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -1,44 +1,17 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def findTarget(self, root, k):
-        """
-        :type root: Optional[TreeNode]
-        :type k: int
-        :rtype: bool
-        """
-        if root is None:
-            return False
-        if root.left is None and root.right is None:
-            return False
-        q=deque()
-        q.append(root)
-        def hasnode(root,new,curr):
-            if root is None:
-                return False
-            if root.val==new:
-                return root!=curr 
-            elif new<root.val:
-                return hasnode(root.left,new,curr)
-            elif new>root.val:
-                return hasnode(root.right,new,curr)
-        while q:
-            node=q.popleft()
-            value=k-node.val
-            
-            if hasnode(root,value,node):
-                return True 
-            if node.left:
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
-        return False
-            
-            
 
-            
-        
+        seen = set()
+
+        def dfs(node):
+            if not node:
+                return False
+
+            if k - node.val in seen:
+                return True
+
+            seen.add(node.val)
+
+            return dfs(node.left) or dfs(node.right)
+
+        return dfs(root)
