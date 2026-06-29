ba2826d04ce2a669d78af62422474d8e294115d0
Midde Jahnavi <mjahnavi956@gmail.com>
2026-06-29 20:54:40 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -6,18 +6,26 @@ class Solution(object):
         """
 
 
-        def helper(ind,dp):
-            if  ind>len(nums)-1:
+        # def helper(ind,dp):
+        #     if  ind>len(nums)-1:
                 
-                return 0
-            if dp[ind]!=-1:
-                return dp[ind]
-            else:
-                take=nums[ind]+helper(ind+2,dp)
-                nottake=helper(ind+1,dp)
-                ans=max(take,nottake)
-                dp[ind]=ans
-                return dp[ind]
-        fs=helper(0,[-1]*(len(nums)))
+        #         return 0
+        #     if dp[ind]!=-1:
+        #         return dp[ind]
+        #     else:
+        #         take=nums[ind]+helper(ind+2,dp)
+        #         nottake=helper(ind+1,dp)
+        #         ans=max(take,nottake)
+        #         dp[ind]=ans
+        #         return dp[ind]
+        # fs=helper(0,[-1]*(len(nums)))
         
-        return fs
+        # return fs
+
+        dp=[-1]*(len(nums)+2)
+        n=len(nums)
+        dp[n]=0
+        dp[n+1]=0
+        for i in range(n-1,-1,-1):
+            dp[i]=max(dp[i+1],nums[i]+dp[i+2])
+        return dp[0]
