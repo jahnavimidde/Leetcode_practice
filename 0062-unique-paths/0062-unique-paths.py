d43bbd1ed364aa988390008df7a63b22cfee545d
Midde Jahnavi <mjahnavi956@gmail.com>
2026-06-30 21:42:13 +0530

    Time: 4 ms (12.11%), Space: 12.4 MB (27.98%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -5,26 +5,51 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [[-1 for _ in range(n)] for _ in range(m)]
-        dp[m-1][n-1]=1
+        # dp = [[-1 for _ in range(n)] for _ in range(m)]
+        # dp[m-1][n-1]=1
 
-        def helper(r,d,dp):
-            if r>m-1:
-                return 0
-            if d>n-1:
-                return 0
+        # def helper(r,d,dp):
+        #     if r>m-1:
+        #         return 0
+        #     if d>n-1:
+        #         return 0
 
-            if dp[r][d]!=-1:
-                return dp[r][d]
+        #     if dp[r][d]!=-1:
+        #         return dp[r][d]
             
 
             
-            right=helper(r+1,d,dp)
-            down=helper(r,d+1,dp)
-            dp[r][d]=right+down
-            return dp[r][d]
-        ans=helper(0,0,dp)
-        return ans
+        #     right=helper(r+1,d,dp)
+        #     down=helper(r,d+1,dp)
+        #     dp[r][d]=right+down
+        #     return dp[r][d]
+        # ans=helper(0,0,dp)
+        # return ans
+
+
+
+
+        dp = [[0] * n for _ in range(m)]
+
+        dp[m-1][n-1] = 1
+
+        for r in range(m-1, -1, -1):
+            for d in range(n-1, -1, -1):
+                if r == m-1 and d == n-1:
+                    continue
+
+                right = dp[r][d+1] if d+1 < n else 0
+                down = dp[r+1][d] if r+1 < m else 0
+
+                dp[r][d] = right + down
+
+        return dp[0][0]
+
+
+            
+            
+            
+        
             
                 
         
