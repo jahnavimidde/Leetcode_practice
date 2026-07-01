1fed691490009095206a68cd8696356e2f14b89f
Midde Jahnavi <mjahnavi956@gmail.com>
2026-07-01 18:55:03 +0530

    Time: 1 ms (34.98%), Space: 12.5 MB (28.14%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -28,22 +28,49 @@ class Solution(object):
 
 
 
+#tabulation
+        # dp = [[0] * (n) for _ in range(m)]
 
-        dp = [[0] * n for _ in range(m)]
+        # dp[m-1][n-1] = 1
 
-        dp[m-1][n-1] = 1
+        # for r in range(m-1, -1, -1):
+        #     for d in range(n-1, -1, -1):
+        #         if r == m-1 and d == n-1:
+        #             continue
 
-        for r in range(m-1, -1, -1):
-            for d in range(n-1, -1, -1):
-                if r == m-1 and d == n-1:
-                    continue
+        #         right = dp[r][d+1] if d+1 < n else 0
+        #         down = dp[r+1][d] if r+1 < m else 0
 
-                right = dp[r][d+1] if d+1 < n else 0
-                down = dp[r+1][d] if r+1 < m else 0
+        #         dp[r][d] = right + down
 
-                dp[r][d] = right + down
+        # return dp[0][0]
 
-        return dp[0][0]
+
+# space optimisation
+# to compute the cell we only need prev cell and above cell  
+# prev cell is obtained by current row,and above cell is obtained by maintaing the prev row values (that is 1D array)
+
+
+#How many ways are there to reach the cell? (that is what each cell represents )  
+        prev=[0]*n
+
+        for i in range(m):
+            temp=[-1 for _ in range(n)]
+            for j in range(n):
+                
+                if i==0 and j==0:
+                    temp[j]=1 #there is only one way to reach the start
+                else:
+                    above=prev[j] 
+                    pre=temp[j-1] if j>0 else 0
+                    temp[j]=above+pre
+            prev=temp
+        return temp[n-1]
+        
+                    
+
+
+
 
 
             
