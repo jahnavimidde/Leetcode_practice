492feb8f76b0e1de36783155d9c0b1a0abd3916a
Midde Jahnavi <mjahnavi956@gmail.com>
2026-07-02 13:49:03 +0530

    Time: 18 ms (39.48%), Space: 12.9 MB (95.92%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -21,25 +21,50 @@ class Solution(object):
         # mini=helper(0,0)
         # return mini
 
-        dp = [[-1] * len(grid[0]) for _ in range(len(grid))]
 
-        def helper(r, d):
-            if r == len(grid)-1 and d == len(grid[0])-1:
-                return grid[r][d]
+        #memoization
 
-            if r >= len(grid) or d >= len(grid[0]):
-                return float('inf')
+        # dp = [[-1] * len(grid[0]) for _ in range(len(grid))]
 
-            if dp[r][d] != -1:
-                return dp[r][d]
+        # def helper(r, d):
+        #     if r == len(grid)-1 and d == len(grid[0])-1:
+        #         return grid[r][d]
 
-            right = helper(r, d+1)
-            down = helper(r+1, d)
+        #     if r >= len(grid) or d >= len(grid[0]):
+        #         return float('inf')
 
-            dp[r][d] = grid[r][d] + min(right, down)
-            return dp[r][d]
+        #     if dp[r][d] != -1:
+        #         return dp[r][d]
 
-        return helper(0, 0)
+        #     right = helper(r, d+1)
+        #     down = helper(r+1, d)
+
+        #     dp[r][d] = grid[r][d] + min(right, down)
+        #     return dp[r][d]
+
+        # return helper(0, 0)
+
+        prev = [float('inf')] * len(grid[0])
+
+        for i in range(len(grid)):
+            temp = [float('inf')] * len(grid[0])
+
+            for j in range(len(grid[0])):
+
+                if i == 0 and j == 0:
+                    temp[j] = grid[i][j]
+                else:
+                    up = prev[j]
+                    left = temp[j-1] if j > 0 else float('inf')
+
+                    temp[j] = grid[i][j] + min(up, left)
+
+            prev = temp      # ← Missing line
+
+        return prev[-1]
+
+
+
             
         
 
