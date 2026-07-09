4a74a46c63f3fae129cf68f9ccdf8b5ee01fa3b8
Midde Jahnavi <mjahnavi956@gmail.com>
2026-07-09 23:13:37 +0530

    Time: 44 ms (45.9%), Space: 12.3 MB (70.49%) - LeetHub

---
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def canPartitionKSubsets(self, nums, k):
+
+        total = sum(nums)
+
+        if total % k != 0:
+            return False
+
+        target = total // k
+
+        nums.sort(reverse=True)
+
+        buckets = [0] * k
+
+        def dfs(i):
+
+            if i == len(nums):
+                return True
+
+            for j in range(k):
+
+                if buckets[j] + nums[i] <= target:
+
+                    buckets[j] += nums[i]
+
+                    if dfs(i + 1):
+                        return True
+
+                    buckets[j] -= nums[i]
+
+                # Pruning: don't try other empty buckets with the same state
+                if buckets[j] == 0:
+                    break
+
+            return False
+
+        return dfs(0)
