edb6f4a714ec1b27a3628f52d34d273937516dc4
Midde Jahnavi <mjahnavi956@gmail.com>
2026-07-11 23:41:40 +0530

    Time: 1925 ms (5.85%), Space: 17.3 MB (15.89%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -11,41 +11,28 @@ class Solution(object):
             return -1
         if len(coins)==1 and coins[0]>amount:
             return -1
-        def helper(ind,total,dp):
-            
-            
-            if ind<0:
-                return float('inf')
-            if total==0:
-                return 0
-            
-            if ind==0 and total%coins[ind]==0:
-                return total//coins[ind]
-            if dp[ind][total]!=-1:
-                return dp[ind][total]
-            
-            nt=helper(ind-1,total,dp)
-            
-            t=float('inf')
-            
-                
-                
-            if coins[ind] <= total:
-              t=1+helper(ind,total-coins[ind],dp)
-            
-            dp[ind][total]=min(t,nt)
-            return dp[ind][total]
         dp = [[-1 for _ in range(amount + 1)] for _ in range(len(coins))]
         for i in range(len(coins)):
             dp[i][0]=0
         for i in range(amount+1):
             if i%coins[0]==0:
                dp[0][i]=i//coins[0]
-        ans = helper(len(coins)-1, amount, dp)
+            else:
+                dp[0][i]=float('inf')
+        for i in range(1,len(coins)):
+            for j in range(1,amount+1):
+                
+                nt=dp[i-1][j]
+                t=float('inf')
+                if coins[i] <= j:
+                   t=1+dp[i][j-coins[i]]
+                dp[i][j]=min(t,nt)
+        ans = dp[len(coins)-1][amount]
+        return -1 if ans == float('inf') else ans
+        
+        
 
-        if ans == float('inf'):
-            return -1
-        return ans
+        
                 
 
         
