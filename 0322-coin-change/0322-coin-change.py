4f324f0034bc405a060c54db7a1392aac17229ff
Midde Jahnavi <mjahnavi956@gmail.com>
2026-07-11 23:53:54 +0530

    Time: 1805 ms (7.16%), Space: 13.8 MB (20.32%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -11,23 +11,25 @@ class Solution(object):
             return -1
         if len(coins)==1 and coins[0]>amount:
             return -1
-        dp = [[-1 for _ in range(amount + 1)] for _ in range(len(coins))]
-        for i in range(len(coins)):
-            dp[i][0]=0
+        prev=[-1 for _ in range(amount + 1)]
+        
         for i in range(amount+1):
             if i%coins[0]==0:
-               dp[0][i]=i//coins[0]
+               prev[i]=i//coins[0]
             else:
-                dp[0][i]=float('inf')
+                prev[i]=float('inf')
         for i in range(1,len(coins)):
+            curr=[-1 for _ in range(amount+1)]
+            curr[0]=0
             for j in range(1,amount+1):
                 
-                nt=dp[i-1][j]
+                nt=prev[j]
                 t=float('inf')
                 if coins[i] <= j:
-                   t=1+dp[i][j-coins[i]]
-                dp[i][j]=min(t,nt)
-        ans = dp[len(coins)-1][amount]
+                   t=1+curr[j-coins[i]]
+                curr[j]=min(t,nt)
+            prev=curr
+        ans = prev[amount]
         return -1 if ans == float('inf') else ans
         
         
