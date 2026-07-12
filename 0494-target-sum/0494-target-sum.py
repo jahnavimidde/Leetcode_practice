51e2858af8d574d5159c72b2c0615811384989b3
Midde Jahnavi <mjahnavi956@gmail.com>
2026-07-12 19:54:05 +0530

    Time: 52 ms (71.51%), Space: 12.4 MB (88.01%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -18,33 +18,56 @@ class Solution(object):
                 
             
            
-        def helper(ind,tar,dp):
+        # def helper(ind,tar,dp):
             
-            if ind==0:
-                if nums[0]==0 and tar==0:
-                    return 2
-                elif nums[0]==tar or tar==0:
-                    return 1
-                else:
-                    return 0
-            if dp[ind][tar]!=-1:
-                return dp[ind][tar]
+        #     if ind==0:
+        #         if nums[0]==0 and tar==0:
+        #             return 2
+        #         elif nums[0]==tar or tar==0:
+        #             return 1
+        #         else:
+        #             return 0
+        #     if dp[ind][tar]!=-1:
+        #         return dp[ind][tar]
 
-            nt=helper(ind-1,tar,dp)
-            t=0
-            if tar>=nums[ind]:
-                t=helper(ind-1,tar-nums[ind],dp)
-            dp[ind][tar]=t+nt
-            return dp[ind][tar]
+            # nt=helper(ind-1,tar,dp)
+            # t=0
+            # if tar>=nums[ind]:
+            #     t=helper(ind-1,tar-nums[ind],dp)
+            # dp[ind][tar]=t+nt
+            # return dp[ind][tar]
+
+
+        tar=(sum(nums)-target)//2 
+        prev =[-1 for _ in range(tar+1)]
+        for j in range(tar+1):
+            
+            if nums[0]==0 and j==0:
+                prev[j]=2
+            elif nums[0]==j or j==0:
+                prev[j]=1
+            else:
+                prev[j]=0
+        for i in range(1,len(nums)):
+            curr=[-1 for _ in range(tar+1)] 
+            for j in range(tar+1):
+                nt=prev[j]
+                t=0
+                if j>=nums[i]:
+                    t=prev[j-nums[i]]
+                curr[j]=nt+t
+            prev=curr
+
             
                 
             
            
 
-        tar=(sum(nums)-target)//2 
-        dp=[[-1 for _ in range(tar+1)] for _ in range(len(nums))]
-        ans=helper(len(nums)-1,tar,dp)
-        return ans 
+        
+        return prev[tar]
+            
 
+        
+        
         
         
