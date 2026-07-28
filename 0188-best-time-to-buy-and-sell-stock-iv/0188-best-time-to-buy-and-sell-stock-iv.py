a1ffe91d64d6d41bc39643e082380f7e9e5a5155
Midde Jahnavi <mjahnavi956@gmail.com>
2026-07-29 00:02:26 +0530

    Time: 131 ms (15.7%), Space: 16.7 MB (17.15%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -6,28 +6,59 @@ class Solution(object):
         :rtype: int
         """
 
-        def helper(ind,buy,cap,dp):
+        # def helper(ind,buy,cap,dp):
             
-            if ind==len(prices) or cap==0:
-                return 0
-            if dp[ind][buy][cap]!=-1:
-                return dp[ind][buy][cap]
-            take=0
-            nottake=0
-            sell=0
-            notsell=0
+        #     if ind==len(prices) or cap==0:
+        #         return 0
+        #     if dp[ind][buy][cap]!=-1:
+        #         return dp[ind][buy][cap]
+        #     take=0
+        #     nottake=0
+        #     sell=0
+        #     notsell=0
 
-            if buy: #if buy==1 it means there are no stocks in hand which are bought
-                take= -(prices[ind])+helper(ind+1,0,cap,dp)
-                nottake=helper(ind+1,1,cap,dp)
+        #     if buy: #if buy==1 it means there are no stocks in hand which are bought
+        #         take= -(prices[ind])+helper(ind+1,0,cap,dp)
+        #         nottake=helper(ind+1,1,cap,dp)
 
-            else: 
-                #if stocks are bought then sell (sell,notsell)
-                sell=prices[ind]+helper(ind+1,1,cap-1,dp)
-                notsell=helper(ind+1,0,cap,dp)
-            dp[ind][buy][cap]=max(take,nottake,sell,notsell)
-            return max(take,nottake,sell,notsell)
-        dp=[[[-1 for _ in range(k+1)]for _ in range(2)] for _ in range(len(prices))]
-        ans=helper(0,1,k,dp)
-        return ans
+        #     else: 
+        #         #if stocks are bought then sell (sell,notsell)
+        #         sell=prices[ind]+helper(ind+1,1,cap-1,dp)
+        #         notsell=helper(ind+1,0,cap,dp)
+        #     dp[ind][buy][cap]=max(take,nottake,sell,notsell)
+        #     return max(take,nottake,sell,notsell)
+        # dp=[[[-1 for _ in range(k+1)]for _ in range(2)] for _ in range(len(prices))]
+        # ans=helper(0,1,k,dp)
+        # return ans
+
+
+        # for 
+
+
+
+        dp=[[[0 for _ in range(k+1)]for _ in range(2)] for _ in range(len(prices)+1)]
+
+        for ind in range(len(prices)-1,-1,-1):
+            for buy in range(1,-1,-1):
+                for cap in range(k,0,-1):
+                    take=0
+                    nottake=0
+                    sell=0
+                    notsell=0
+
+                    if buy: #if buy==1 it means there are no stocks in hand which are bought
+                        take= -(prices[ind])+dp[ind+1][0][cap]
+                        nottake=dp[ind+1][1][cap]
+
+                    else: 
+                        #if stocks are bought then sell (sell,notsell)
+                        sell=prices[ind]+dp[ind+1][1][cap-1]
+                        notsell=dp[ind+1][0][cap]
+                    dp[ind][buy][cap]=max(take,nottake,sell,notsell)
+        return dp[0][1][k]
+
+
+
+
+
         
