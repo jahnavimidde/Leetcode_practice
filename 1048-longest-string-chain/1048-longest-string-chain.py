0ee753a0a9b7eb221a8a6628cb60a3e49428e003
Midde Jahnavi <mjahnavi956@gmail.com>
2026-08-01 00:21:30 +0530

    Time: 1627 ms (23.37%), Space: 12.6 MB (30.52%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,10 +1,14 @@
 class Solution(object):
     def longestStrChain(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
 
         words.sort(key=len)
         n = len(words)
 
-        dp = [[-1 for _ in range(n + 1)] for _ in range(n)]
+        dp = [1] * n
 
         def isPred(a, b):
             if len(b) != len(a) + 1:
@@ -19,21 +23,9 @@ class Solution(object):
 
             return i == len(a)
 
-        def helper(ind, prev):
+        for ind in range(n):
+            for prev in range(ind):
+                if isPred(words[prev], words[ind]):
+                    dp[ind] = max(dp[ind], dp[prev] + 1)
 
-            if ind == n:
-                return 0
-
-            if dp[ind][prev + 1] != -1:
-                return dp[ind][prev + 1]
-
-            nottake = helper(ind + 1, prev)
-
-            take = 0
-            if prev == -1 or isPred(words[prev], words[ind]):
-                take = 1 + helper(ind + 1, ind)
-
-            dp[ind][prev + 1] = max(take, nottake)
-            return dp[ind][prev + 1]
-
-        return helper(0, -1)
+        return max(dp)
