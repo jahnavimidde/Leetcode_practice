9f2254927b00fff2bedde5691a71483cfc41b38e
Midde Jahnavi <mjahnavi956@gmail.com>
2026-08-17 19:31:05 +0530

    Time: 909 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,6 +1,20 @@
-SELECT MAX(e.salary) AS SecondHighestSalary
-FROM Employee e
-WHERE e.salary < (
-    SELECT MAX(e2.salary)
-    FROM Employee e2
+select max(e.salary) as SecondHighestSalary
+from employee e
+where exists(
+    select 1
+    from employee e2
+    where e2.salary>e.salary
+) and not exists(
+    select 1 
+    from employee e3
+    where e3.salary>e.salary
+    and exists(
+        select 1 
+        from employee e4
+        where e4.salary>e3.salary
+    )
 );
+
+
+
+#  Aggregate functions always return one row;   so use max(e.salary)
